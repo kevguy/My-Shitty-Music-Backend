f580fd77ab1e1102426db8e136ed01bd991dfa97
Kev Lai <kevlai22@uw.edu>
2018-05-20 10:22:07 +0800

    feat: add first simple websocket

---
+++ references/websocket.txt
@@ -0,0 +1,1 @@
+https://jacobmartins.com/2016/03/07/practical-golang-using-websockets/
---
+++ websocket/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	indexFile, err := os.Open("html/index.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+	index, err := ioutil.ReadAll(indexFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	http.HandleFunc("/websocket", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		for {
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			if string(msg) == "ping" {
+				fmt.Println("ping")
+				time.Sleep(2 * time.Second)
+				err = conn.WriteMessage(msgType, []byte("pong"))
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+			} else {
+				conn.Close()
+				fmt.Println(string(msg))
+				return
+			}
+		}
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, string(index))
+	})
+	http.ListenAndServe(":3000", nil)
+}
